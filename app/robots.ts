de7acc6ed8973154fcf49401f8feba3793e0ce1b
marco_ <elom.karl.patrick@gmail.com>
2026-09-23 11:50:17 +0000

    fix: avoid placeholder sitemap URL in robots

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,6 @@ import { site } from "@/lib/site";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: `${site.url}/sitemap.xml`,
+    ...(site.url ? { sitemap: `${site.url}/sitemap.xml` } : {}),
   };
 }
